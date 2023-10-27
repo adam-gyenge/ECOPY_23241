f9adfb6c94e9bf0e7e3eb9a6c07b28daa8e94d6e
adam-gyenge <gyengeadam99@gmail.com>
2023-10-27 14:22:22 +0200

    Update LinearRegressions.py

--- src/linear_regression/LinearRegressions.py
+++ src/linear_regression/LinearRegressions.py
@@ -3,21 +3,6 @@ import pandas as pd
 import typing
 import statsmodels.api as sm
 
-sp500 = pd.read_parquet('C:\DESKTOP\PythonEconRepoOnDesktop\ECOPY_23241\data\sp500.parquet', engine='fastparquet')
-ff_factors = pd.read_parquet(r'C:\DESKTOP\PythonEconRepoOnDesktop\ECOPY_23241\data\ff_factors.parquet', engine='fastparquet')
-
-df = sp500.merge(ff_factors, left_on='Date', right_on='Date')
-
-df['Excess_Return'] = df['Monthly Returns']-df['RF']
-
-df.sort_values(by='Date', inplace=True)
-df['ex_ret_1'] = df.groupby('Symbol')['Excess_Return'].shift(-1)
-
-df.dropna(subset=['ex_ret_1'], inplace=True)
-df.dropna(subset=['HML'], inplace=True)
-
-amzn_df = df[df.Symbol == 'AMZN']
-amzn_df.drop(columns=["Symbol"], inplace=True)
 
 # LINEAR MODEL
 class LinearRegressionSM:
