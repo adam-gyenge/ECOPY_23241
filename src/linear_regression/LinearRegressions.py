a858e73ef04dbb836a5cd7fdba58029c08518201
adam-gyenge <gyengeadam99@gmail.com>
2023-11-11 18:13:20 +0100

    Update LinearRegressions.py

--- src/linear_regression/LinearRegressions.py
+++ src/linear_regression/LinearRegressions.py
@@ -3,7 +3,8 @@ import pandas as pd
 import typing
 import numpy as np
 import statsmodels.api as sm
-from scipy.stats import t
+from scipy.stats import t, f
+from scipy import stats
 
 
 # LINEAR MODEL
@@ -57,44 +58,69 @@ class LinearRegressionNP:
     def __init__(self, left_hand_side, right_hand_side):
         self.left_hand_side = left_hand_side
         self.right_hand_side = right_hand_side
-        self.beta = None
-        self.p_values = None
-        self.centered_r_squared = None
-        self.adjusted_r_squared = None
-        self.wald_statistic = None
+        self.beta = None  # Placeholder for estimated coefficients
 
     def fit(self):
-        X = sm.add_constant(self.right_hand_side)
-        model = sm.OLS(self.left_hand_side, X).fit()
-        self.beta = model.params
-        self._model = model
+        # Add a constant to the right-hand side for intercept term
+        self.X = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
+
+        # Perform OLS regression
+        self.beta = np.linalg.lstsq(self.X, self.left_hand_side, rcond=None)[0]
 
     def get_params(self):
-        return pd.Series(self.beta, name="Beta coefficients")
+        if self.beta is not None:
+            return pd.Series(self.beta, name='Beta coefficients')
+        else:
+            raise ValueError("Model has not been fitted. Call fit() first.")
 
     def get_pvalues(self):
-        if self._model is not None:
-            params = self._model.pvalues
-            params = params.rename('P-values for the corresponding coefficients')
-            return params
-        else:
-            raise ValueError("Fit the model first to get parameters.")
+        if self.beta is not None:
+            n = len(self.left_hand_side)
+            k = len(self.beta)
 
-    def get_wald_test_result(self, restriction_matrix):
-        if self._model is not None:
-            wald_test_result = self._model.wald_test(restriction_matrix)
-            fvalue = wald_test_result.statistic[0][0]
-            pvalue = wald_test_result.pvalue
-            return f"Wald: {fvalue:.3f}, p-value: {pvalue:.3f}"
-        else:
-            raise ValueError("Fit the model first to get goodness-of-fit values.")
+            # Calculate t-statistics and corresponding p-values
+            t_stats = self.beta / np.sqrt(np.diag(np.linalg.inv(self.X.T @ self.X)) * (np.sum((self.left_hand_side - self.X @ self.beta)**2) / (n - k)))
 
-    def get_model_goodness_values(self, include_constant=True):
-        n = len(self.left_hand_side)
-        p = len(self.beta) - (1 if include_constant else 0)
-        y_mean = np.mean(self.left_hand_side)
-        tss = np.sum((self.left_hand_side - y_mean) ** 2)
-        rss = np.sum((self.left_hand_side - np.dot(sm.add_constant(self.right_hand_side), self.beta)) ** 2)
-        adj_r_squared = 1 - (rss / (n - p)) / (tss / (n))
-        cen_r_squared = 1 - (rss / (n - p)) / (tss / (n - p))
-        return f"Centered R-squared: {cen_r_squared:.3f}, Adjusted R-squared: {adj_r_squared:.3f}"
+            # Calculate two-tailed p-values
+            p_values = 2 * (1 - t.cdf(np.abs(t_stats), df=n - k))
+
+            return pd.Series(p_values, name='P-values for the corresponding coefficients')
+        else:
+            raise ValueError("Model has not been fitted. Call fit() first.")
+
+    def get_wald_test_result(self, R):
+        if self.beta is not None:
+            one = (np.array(R) @ self.beta).T
+            mid = np.array(R) @ np.linalg.inv(self.X.T @ self.X) @ np.array(R).T
+            end = np.array(R) @ self.beta
+
+            n = len(self.left_hand_side)
+            m, k = np.array(R).shape
+
+            # sigma squared
+            residuals = self.left_hand_side - self.X @ self.beta
+            sigma_squared = np.sum(residuals ** 2) / (n - k)
+
+            wald_value = (one @ np.linalg.inv(mid) @ end) / (m * sigma_squared)
+            p_value = 1 - f.cdf(wald_value, dfn=m, dfd=n - k)
+
+            return f"Wald: {wald_value:.3f}, p-value: {p_value:.3f}"
+        else:
+            raise ValueError("Model has not been fitted. Call fit() first.")
+
+    def get_model_goodness_values(self):
+        if self.beta is not None:
+            n = len(self.left_hand_side)
+            k = len(self.beta)
+
+            # Calculate centered R-squared
+            y_hat = self.X @ self.beta
+            y_bar = np.mean(self.left_hand_side)
+            centered_r_squared = 1 - np.sum((self.left_hand_side - y_hat)**2) / np.sum((self.left_hand_side - y_bar)**2)
+
+            # Calculate adjusted R-squared
+            adjusted_r_squared = 1 - (1 - centered_r_squared) * ((n - 1) / (n - k))
+
+            return f"Centered R-squared: {centered_r_squared:.3f}, Adjusted R-squared: {adjusted_r_squared:.3f}"
+        else:
+            raise ValueError("Model has not been fitted. Call fit() first.")
